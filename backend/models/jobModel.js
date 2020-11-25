f6073b9a88dedcfd3649e3d99b2b1146f2701912
bosstyzaz <wutipong1234@hotmail.com>
2020-11-25 22:16:39 +0700

    apply job create in job controller

---
+++ backend/models/jobModel.js
@@ -0,0 +1,51 @@
+const { Int32 } = require("mongodb");
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+// Crete Schema
+const JobSchema = new Schema({
+  JobName: {
+    type: String,
+    required: true,
+  },
+  JobDetail: {
+    type: String,
+    required: true,
+  },
+  Wages: {
+    type: Number,
+    required: true,
+  },
+  Amount: {
+    type: Number,
+    required: true,
+  },
+  Location: {
+    type: String,
+    required: true,
+  },
+  BeginTime: {
+    type: String,
+    required: true,
+  },
+  EndTime: {
+    type: String,
+    required: true,
+  },
+  Date: {
+    type: String,
+    required: true,
+  },
+  CurrentEmployee: [],
+  CurrentAcceptedEmployee: [],
+  Status: {
+    type: String,
+    required: true,
+  },
+  Employer: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = User = mongoose.model("Jobs", JobSchema, "Jobs");
--- backend/server.js
+++ backend/server.js
@@ -10,8 +10,8 @@ const passport = require("passport");
 console.log("Start server");
 
 // const passport = require("./passport").pp();
-const userRouter = require("./routes/testuserRoutes");
-const jobRouter = require("./routes/testjobsRoutes");
+const userRouter = require("./routes/userRoutes");
+const jobRouter = require("./routes/jobsRoutes");
 const app = express();
 
 // DB Config
@@ -24,7 +24,6 @@ const uri = process.env.DATABASE.replace(
   '<PASSWORD>',
   process.env.DATABASE_PASSWORD
 );
-// const db = require("./config/keys").mongoURI;
 
 // Connect to MongoDB
 mongoose
@@ -83,53 +82,3 @@ app.use("/", (req, res, next) => {
   res.send("Hello WOrld");
 });
 app.listen(8000, () => console.log("server started"));
-
-
-// mongoUtil.connectToServer(function (err, client) {
-//   if (err) console.log(err);
-//   const passport = require("./passport").pp();
-//   const userRouter = require("./routes/userRoutes");
-//   const jobRouter = require("./routes/jobs");
-//   const app = express();
-
-//   app.use(
-//     cors({
-//       origin: ["http://localhost:3000"],
-//       credentials: true,
-//     })
-//   );
-
-//   app.use(express.json());
-//   app.all("*", function (req, res, next) {
-//     res.header("Access-Control-Allow-Origin", "*");
-//     res.header("Access-Control-Allow-Credentials", true);
-//     res.header(
-//       "Access-Control-Allow-Methods",
-//       "PUT, GET, POST, DELETE, OPTIONS"
-//     );
-//     res.header(
-//       "Access-Control-Allow-Headers",
-//       "Origin, X-Requested-With, Content-Type, Accept"
-//     );
-//     next();
-//   });
-//   app.use(
-//     session({
-//       secret: "qve~UV20_0",
-//       resave: false,
-//       saveUninitialized: false,
-//     })
-//   );
-
-//   // passport middleware setup ( it is mandatory to put it after session middleware setup)
-//   app.use(passport.initialize());
-//   app.use(passport.session());
-
-//   //   app.use('/', doctorRouter)
-//   app.use("/user", userRouter);
-//   app.use("/jobs", jobRouter);
-//   app.use("/", (req, res, next) => {
-//     res.send("Hello WOrld");
-//   });
-//   app.listen(8000, () => console.log("server started"));
-// });
